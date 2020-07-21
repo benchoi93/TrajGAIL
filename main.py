74eef1b1940183ddf4e1696a6fd4f3f535cac981
benchoi93 <benchoi93@gmail.com>
2020-07-21 10:35:11 +0900

    os join for paths

--- main.py
+++ main.py
@@ -16,18 +16,27 @@ if args.model == "IRL":
     # IRL Test
     for data0 in datalist:
         print(".\\data\\"+args.data+"\\" + data0)
-        subprocess.run(["python" , '.\\scripts\\irl\\demo_shortestpath.py',"--data",'.\\data\\'+args.data+'\\'+data0+''])
-        # os.system('cmd /k "python    "')
+        subprocess.run(["python" , 
+                        os.path.join('.','scripts','irl','demo_shortestpath.py'),
+                        "--data",
+                        os.path.join('.','data',args.data,data0)
+                        ])
 elif args.model == "MMC":
 # MMC Test
     for data0 in datalist:
         print(".\\data\\"+args.data+"\\" + data0)
-        subprocess.run(["python" , '.\\scripts\\behavior_clone\\run_bc_mmc.py',"--data",'.\\data\\'+args.data+'\\'+data0+''])
-        # os.system('cmd /k "python .\\scripts\\behavior_clone\\run_bc_mmc.py --data ".\\data\\'+args.data+'\\'+data0+'" "')
+        subprocess.run(["python" , 
+                        os.path.join('.','scripts','behavior_clone','run_bc_mmc.py'),
+                        "--data",
+                        os.path.join('.','data',args.data,data0)
+                        ])
 elif args.model == "RNN":
     # RNN Test
     for data0 in datalist:
         print(".\\data\\"+args.data+"\\" + data0)
-        subprocess.run(["python" , '.\\scripts\\behavior_clone\\run_bc_rnn.py',"--data",'.\\data\\'+args.data+'\\'+data0+''])
-        # os.system('cmd /k "python .\\scripts\\behavior_clone\\run_bc_rnn.py --data ".\\data\\'+args.data+'\\'+data0+'" "')
+        subprocess.run(["python" , 
+                        os.path.join('.','scripts','behavior_clone','run_bc_rnn.py'),
+                        "--data",
+                        os.path.join('.','data',args.data,data0)
+                        ])
 
